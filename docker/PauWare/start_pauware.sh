9e37d30ab45df346f7fe2d65454d62700853cce0
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2020-03-11 08:57:45 +0100

    Added XModelling Studio to PauWare IDE

--- docker/PauWare/start_pauware.sh
+++ docker/PauWare/start_pauware.sh
@@ -1,2 +1,3 @@
 xhost +
-docker rm pauware && docker run --name pauware -it -d -e DISPLAY=:0 -v /tmp/.X11-unix:/tmp/.X11-unix pauware:v0.1
+docker rm pauware 
+docker run --name pauware -it -d -e DISPLAY=:0 -v /tmp/.X11-unix:/tmp/.X11-unix pauware:v0.1
